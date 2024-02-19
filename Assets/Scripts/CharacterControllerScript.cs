caa910304e736216a475925e8b607ba06df40c91
kuanyshdreos <kuanyshbai44@gmail.com>
2024-02-19 21:46:49 +0600

    animate

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Не успел дописать времени осталось час, но анимации сделал все)
+    }
+}
